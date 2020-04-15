b16380072050ab26f4614e0ad563cac6edf8da96
zyhang8 <zhongyuhang113@gmail.com>
2020-04-16 00:02:42 +0800

    sing_linklist

---
+++ basis/cmake-build-debug/CMakeFiles/basis.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basis.dir/sing_linklist.cpp.obj"
+  "basis.exe"
+  "basis.exe.manifest"
+  "basis.pdb"
+  "libbasis.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
